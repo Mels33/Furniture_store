31ca391411751b347324d0b927c42936b76b957a
Mels33 <marstamyan0@gmail.com>
2024-05-10 20:27:22 +0400

    added goods

--- main/views.py
+++ main/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from goods.models import Categories
+
 def index(request):
+
+    categories = Categories.objects.all()
 
     context = {
         'title': 'Home - main',
-        'context': 'Furniture store HOME'
-
+        'context': 'Furniture store HOME',
+        'categories': categories
     }
-
     return render(request,'main/index.html', context)
 
 def about(request):
