a1787630579ba2bcc73ae02039393ae23f5fd780
Mels33 <marstamyan0@gmail.com>
2024-05-14 14:36:41 +0400

    added Filter

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,10 +1,19 @@
+from django.utils.http import urlencode
+
 from goods.models import Categories
 from django import template
 
-
 register = template.Library()
+
 
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
 
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kvargs):
+    query = context['request'].GET.dict()
+    query.update(kvargs)
+
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -6,13 +6,23 @@ from goods.models import Products
 
 
 def catalog(request, category_slug) -> HttpResponse:
-
     page = request.GET.get('page', 1)
+
+    on_sale = request.GET.get('on_sale', None)
+
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == "all-products":
         goods = Products.objects.all()
+
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
 
@@ -20,18 +30,18 @@ def catalog(request, category_slug) -> HttpResponse:
 
     context = {
         'title': 'Home - catalog',
-        'goods':  current_page,
+        'goods': current_page,
         'slug_url': category_slug,
     }
 
-    return render(request,'goods/catalog.html', context)
+    return render(request, 'goods/catalog.html', context)
+
 
 def product(request, product_slug) -> HttpResponse:
-
     product = Products.objects.get(slug=product_slug)
 
     context = {
         'product': product
     }
 
-    return render(request,'goods/product.html', context=context)
+    return render(request, 'goods/product.html', context=context)
