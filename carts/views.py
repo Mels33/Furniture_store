003131cca74fda86a41703e86bb59766154cacaf
Mels33 <marstamyan0@gmail.com>
2024-05-20 19:18:29 +0400

    Remove from cart ajax and json response

--- carts/views.py
+++ carts/views.py
@@ -1,21 +1,22 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-
 from carts.models import Cart
 from carts.utils import get_user_carts
+
 from goods.models import Products
 
 
 def cart_add(request):
     product_id = request.POST.get("product_id")
+
     product = Products.objects.get(id=product_id)
+
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
         if carts.exists():
             cart = carts.first()
-
             if cart:
                 cart.quantity += 1
                 cart.save()
@@ -24,20 +25,35 @@ def cart_add(request):
 
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
-    )
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
     response_data = {
         "message": "Product added to basket",
-        "cart_items_html": cart_items_html
+        "cart_items_html": cart_items_html,
     }
 
     return JsonResponse(response_data)
 
+
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, cart_id):
+
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
 
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    return redirect(request.META["HTTP_REFERER"])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Product deleted",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
